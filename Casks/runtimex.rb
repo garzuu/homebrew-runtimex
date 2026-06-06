54023e4000e4d81b1c75cdc15706338d2c5e3e56
Matteo Garzulano <garzu@MBP-di-Matteo.homenet.telecomitalia.it>
2026-06-06 23:06:58 +0200

    fix(cask): set correct SHA256 for v1.0.1

--- Casks/runtimex.rb
+++ Casks/runtimex.rb
@@ -1,6 +1,6 @@
 cask "runtimex" do
   version "1.0.1"
-  sha256 "REPLACE_WITH_SHA256"
+  sha256 "c125da5fbd7eca5a469bbccffeadda1003ffb52be2f4018220928fd816f8154d"
 
   url "https://runtimex.garzuu.com/downloads/RuntimeX-macos.dmg"
   name "RuntimeX"
